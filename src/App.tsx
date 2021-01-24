6d69250ab559758f874f4f12cd1608714d161c69
mornir <jeromepott@hotmail.com>
2021-01-24 18:48:35 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,102 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import toChineseNumeral from 'to-chinese-numeral';
+
+import {
+  Card,
+  Heading,
+  studioTheme,
+  ThemeProvider,
+  TextInput,
+  Container,
+  Label,
+  Menu,
+  MenuItem,
+  Stack,
+  Text,
+  Box,
+  MenuDivider,
+  Layer,
+  Select,
+} from '@sanity/ui';
 
 interface AppProps {}
 
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
   // Return the App component.
+
+  const fonts = [
+    'Noto Sans SC',
+    'ZCOOL KuaiLe',
+    'Kaiti',
+    'Liu Jian Mao Cao',
+    'Ma Shan Zheng',
+  ];
+
+  const startingNumber = 156;
+
+  const [selectedFont, setSelectedFont] = useState(fonts[0]);
+
+  const [inputNumber, setInputNumber] = useState(startingNumber.toString());
+
+  const [chineseNumeral, setChineseNumeral] = useState(
+    toChineseNumeral(startingNumber),
+  );
+
+  function convertToChineseNumeral(event: React.ChangeEvent<HTMLInputElement>) {
+    const numString = event.currentTarget.value;
+    setInputNumber(numString);
+
+    const num = parseFloat(numString);
+    setChineseNumeral(toChineseNumeral(num));
+    try {
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+    <ThemeProvider theme={studioTheme}>
+      <Container width={2}>
+        <Card padding={4}>
+          <Heading as="h1" size={3}>
+            Chinese Numerals {selectedFont}
+          </Heading>
+        </Card>
+        <Card padding={4}>
+          <Label size={4}>
+            Enter number
+            <TextInput
+              fontSize={[2, 2, 3, 4]}
+              padding={[3, 3, 4]}
+              border={true}
+              value={inputNumber}
+              onChange={convertToChineseNumeral}
+            />
+          </Label>
+        </Card>
+        <Card padding={6}>
+          <Text size={4}>{chineseNumeral}</Text>
+        </Card>
+        <Card padding={4}>
+          <Select
+            fontSize={[2, 2, 3, 4]}
+            padding={[3, 3, 4]}
+            space={[3, 3, 4]}
+            onChange={(event: React.ChangeEvent<HTMLSelectElement>) =>
+              setSelectedFont(event.currentTarget.value)
+            }
           >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+            {fonts.map((font) => {
+              return (
+                <option key={font} value={font}>
+                  {font}
+                </option>
+              );
+            })}
+          </Select>
+        </Card>
+      </Container>
+    </ThemeProvider>
   );
 }
 
